a6e3b7d0086f726c7f9592347be808881474adbb
Darren McGhee <darrenmcg94@gmail.com>
2024-02-05 00:26:49 +0000

    Further Function Code Work
    Continued development of the function code
    For some reason, the readClient to view the table is not working
    Connection string is definitely populated, table name is hard coded, so not either of those
    Having issues with local workspace and unable to de-bug locally which is making it much harder
    Will continue trying to debug

--- src/functions/returnViewCount.js
+++ src/functions/returnViewCount.js
@@ -3,24 +3,13 @@ const { TableClient } = require("@azure/data-tables");
 
 const env = {
     readOnly: {
-        connectionString: process.env.COSMOSDB_CONNECTION_READONLY,
+        connectionString: process.env["COSMOSDB_CONNECTION_READONLY"],
         tableName: "website_visitor_count"
     },
     readWrite: {
-        connectionString: process.env.COSMOSDB_CONNECTION_READWRITE,
+        connectionString: process.env["COSMOSDB_CONNECTION_READWRITE"],
         tableName: "website_visitor_count"
     }  
-};
-
-const client = {
-    read: TableClient.fromConnectionString(
-        env.readOnly.connectionString,
-        env.readOnly.tableName
-    ),
-    write: TableClient.fromConnectionString(
-        env.readWrite.connectionString,
-        env.readWrite.tableName
-    )
 };
 
 const readClient = TableClient.fromConnectionString(
@@ -37,23 +26,15 @@ app.http('returnViewCount', {
     methods: ['GET'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        try {
-            context.log(`Http function processed request for url "${request.url}"`);
+        context.log(`Http function processed request for url "${request.url}"`);
 
-            var viewCountRecord = await readClient.listEntities();
+        var viewCountRecord = readClient.listEntities();     
 
-            if (!viewCountRecord) {
-                await writeClient.upsertEntity({"PartitionKey": "cv-website", "RowKey": "1", "Count": 1});
-                viewCountRecord = await readClient.listEntities();
-            };
-
-            return { body: viewCountRecord };
-        } catch (error) {
-            context.log.error(error);
-            return {
-                status: 500,
-                body: { error: 'Internal Server Error' }
-            };
+        if (!viewCountRecord) {
+            await writeClient.upsertEntity({"PartitionKey": "cv-website", "RowKey": "1", "Count": 1});
+            viewCountRecord = await readClient.listEntities();
         };
+
+        return { body: viewCountRecord };
     }
 });
